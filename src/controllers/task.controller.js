b7cddeece6e6cb27bcbb1dcb7841ffa97fbd8206
isac1708 <isac.figueredo@gmail.com>
2025-03-04 22:11:00 -0300

    feat: move get by id,creatPost,updatePost,deletePost endPoint to a controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -15,6 +15,67 @@ class TaskController{
             this.res.status(500).send(error.message);
         }
     }
+
+    async getTaskById(req,res){
+        try{
+            const task = await TaskModel.findById(req.params.id);
+            if(!task){
+                this.res.status(404).send('Task not found');
+            }
+            this.res.send(task);
+        }catch(error){
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async createTask(req,res){
+        try{
+            const task = new TaskModel(req.body);
+            await task.save();
+            this.res.status(201).send(task);
+        }catch(error){
+            this.res.status(400).send(error.message);
+        }
+    }
+
+    async updateTask(req,res){
+        try{
+            const taskId=req.params.id;
+            const taskData=req.body;
+    
+            const taskUpdate = await TaskModel.findById(taskId);
+            const allowedUpdates = ['isCompleted']; //campos que podem ser atualizados
+            const requestUpdates = Object.keys(taskData);
+    
+            for(update of requestUpdates) {
+                if(allowedUpdates.includes(update)){
+                    taskUpdate[update] = taskData[update];
+                }
+                else{
+                    return this.res.status(400).send({error: 'Invalid update!'});
+                }
+            }//atualiza os campos permitidos
+    
+            await taskUpdate.save();
+            return this.res.status(200).send(taskUpdate);
+    
+        }catch(error){
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async deleteTask(req,res){
+        try{
+            const task = await TaskModel.findByIdAndDelete(req.params.id);
+            if(!task){
+                this.res.status(404).send('Task not found');
+            }
+            this.res.status(204).send(task);
+        }catch(error){
+            this.res.status(500).send(error.message);
+        }
+    }
+
 }
 
 
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,71 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 
-const TaskModel = require('../models/task.model');
+
 const TaskController = require('../controllers/task.controller');
 
 router.get('/', async (req, res) => {
-   return TaskController.getTasks(req, res);
- });
+   return new TaskController(req, res).getTasks();
+ });//busca todas as tasks
 
  router.get('/:id', async (req, res) => {
-     try{
-         const task = await TaskModel.findById(req.params.id);
-         if(!task){
-             res.status(404).send('Task not found');
-         }
-         res.send(task);
-     }catch(error){
-         res.status(500).send(error.message);
-     }
+       return new TaskController(req, res).getTaskById();
  });//busca uma task pelo id
  
  router.post('/', async (req, res) => {
-    try{
-        const task = new TaskModel(req.body);
-        await task.save();
-        res.status(201).send(task);
-    }catch(error){
-     res.status(400).send(error.message);
-    }
+       return new TaskController(req, res).createTask();
  });//cria uma nova task
  
  router.patch('/:id', async (req, res) => {
-     try{
-         const taskId=req.params.id;
-         const taskData=req.body;
- 
-         const taskUpdate = await TaskModel.findById(taskId);
-         const allowedUpdates = ['isCompleted']; //campos que podem ser atualizados
-         const requestUpdates = Object.keys(taskData);
- 
-         for(update of requestUpdates) {
-             if(allowedUpdates.includes(update)){
-                 taskUpdate[update] = taskData[update];
-             }
-             else{
-                 return res.status(400).send({error: 'Invalid update!'});
-             }
-         }//atualiza os campos permitidos
- 
-         await taskUpdate.save();
-         return res.status(200).send(taskUpdate);
- 
-     }catch(error){
-         res.status(500).send(error.message);
-     }
+    return new TaskController(req, res).updateTask();
  });//atualiza uma task
  
  router.delete('/:id', async (req, res) => {
-     try{
-         const task = await TaskModel.findByIdAndDelete(req.params.id);
-         if(!task){
-             res.status(404).send('Task not found');
-         }
-         res.status(204).send(task);
-     }catch(error){
-         res.status(500).send(error.message);
-     }
+    return new TaskController(req, res).deleteTask();
  });//deleta uma task
 
 module.exports = router;
